5a88be062c26a12ba2e49dd14470d9e20b82a66e
Kirtiman Singh <kirt93@hotmail.com>
2019-01-08 15:10:52 +0000

    Store bookmarks in local storage

--- js/main.js
+++ js/main.js
@@ -13,15 +13,33 @@ function saveBookmark(e) {
     url: siteUrl
   }
   
+  /*
   // Local Storage Test 
-      /*
-      - Local storage stores strings therefore we mush parse the json into a       string in order to save it and when we need to get it back we can parse    it back to json.
-      */
-  localStorage.setItem('test', 'Hello World');
-  console.log(localStorage.getItem('test'));
-  localStorage.removeItem('test');
-  console.log(localStorage.getItem('test'));
+    - Local storage stores strings therefore we mush parse                         the json into a string in order to save it and when we                       need to get it back we can parse it back to json.
+    - We want to save the bookmark in local storage but before we do that we       want to see if there is already any bookmark values stored. If there is,     we want to fetch it, add to it and save it again. 
+      
+      localStorage.setItem('test', 'Hello World');
+      console.log(localStorage.getItem('test'));
+      localStorage.removeItem('test');
+      console.log(localStorage.getItem('test'));
+  */
 
+  // Test if bookmark is null
+  if (localStorage.getItem('bookmarks') === null) {
+    // Initialize empty array
+    var bookmarks = [];
+    // Add to array
+    bookmarks.push(bookmark);
+    // Set to localStorage (JSON.stringify stores bookmark as a string formatted    json array - json to string)
+    localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+  } else {
+    // Get bookmarks from localStorage (JSON.parse will turn a string into json.)
+    var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+    // Add bookmark to array
+    bookmarks.push(bookmark);
+    // Reset back to localStorage
+    localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+  }
   
   // Prevent form from submitting
   e.preventDefault();
