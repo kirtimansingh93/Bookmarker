4e5050055b6412e5de6e035a8f59fdf5a4d2fa5a
Kirtiman Singh <kirt93@hotmail.com>
2019-01-08 14:24:46 +0000

    Added event listner to submit button. Console logging siteName and siteUrl values on submit.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,16 @@
 // Listen for form submit
 document.getElementById('myForm').addEventListener('submit', saveBookmark)
 
+// Save Bookmark
 function saveBookmark(e) {
-  console.log('It Works!');
+  // Get  from values
+  var siteName = document.getElementById('siteName').value;
+  var siteUrl = document.getElementById('siteUrl').value;
+  console.log(siteName);
+  console.log(siteUrl);
+  
+  
+
+  // Prevent form from submitting
   e.preventDefault();
 }
